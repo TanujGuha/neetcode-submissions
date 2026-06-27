305d61ffdaaaca23be373a7a0c49a7c24e23864e
TanujGuha <162046516+TanujGuha@users.noreply.github.com>
2026-06-27 21:28:24 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size() ; 
+
+
+        int ts = accumulate(nums.begin() , nums.end() , 0) ; 
+
+
+        return n*(n+1)/2 - ts ;
+    }
+};
