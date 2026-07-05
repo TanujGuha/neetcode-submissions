2502f866e61853d6d5a5a9a20a0caab79f006c20
TanujGuha <162046516+TanujGuha@users.noreply.github.com>
2026-07-05 14:36:41 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if (intervals.empty()) return true ; 
+        sort(intervals.begin(), intervals.end(), [](const Interval& a, const Interval& b) {
+            return a.start < b.start;
+        });
+        int end = intervals[0].end ; 
+
+        int n = intervals.size() ;
+        for(int i = 1 ; i < n ; i++)
+        {
+            if (intervals[i].start >= end)
+            {
+                end = intervals[i].end ; 
+            }
+            else
+            {
+                return false ; 
+            }
+        }
+
+        return true ; 
+    }
+};
